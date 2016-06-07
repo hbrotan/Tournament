0850da4e39d6d563f8f0007fa8b4b51fb21f3bef
Hallstein <hbrotan@gmail.com>
2016-06-07 20:08:06 +0200

    cleaning up

--- angular.js
+++ angular.js
@@ -1,73 +1,78 @@
 (function () {
     'use strict';
-	
+
     angular
         .module('app', ['ngRoute'])
         .config(appConfig)
         .controller('controller', controller)
         .factory('dataservice', dataservice);
-                
-        controller.$inject = ['$rootScope', '$location', 'dataservice'];
-        dataservice.$inject = ['$q','$http'];    
 
-	function appConfig($routeProvider, $locationProvider){
-		$locationProvider.html5Mode(true);	
-	}
+    controller.$inject = ['$rootScope', '$location', 'dataservice'];
+    dataservice.$inject = ['$q', '$http'];
 
-        function controller($rootScope, $location, dataservice) {
-            var vm = this;
-            
-            getDataBasedOnRouteParams();
-            registerEventHandlers();
-            
-            function registerEventHandlers(){
-                $rootScope.$on('$locationChangeSuccess', function () {
-        		getDataBasedOnRouteParams();
-    		});
-            }
-            
-            function getDataBasedOnRouteParams(){
-	            vm.league = $location.path().split('/')[1];
-	           
-	           
-	           
-	            if (vm.league){
-	                dataservice.getResultForLeague(vm.league)
-	                    .then(function(data){
-	                            vm.results = angular.fromJson(data[0].Result);
-					vm.calculated = data[0].CalculatedAt;
-	                        });;
-	            } else{
-	                dataservice.getLeagues()
-	                    .then(function(data){
-	                        vm.leagues = data;
-	                    });
-	            }     
+    function appConfig($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
+
+    function controller($rootScope, $location, dataservice) {
+        var vm = this;
+        vm.isBusy = true;
+
+        getDataBasedOnRouteParams();
+        registerEventHandlers();
+
+        function registerEventHandlers() {
+            $rootScope.$on('$locationChangeSuccess', function () {
+                getDataBasedOnRouteParams();
+            });
+        }
+
+        function getDataBasedOnRouteParams() {
+            vm.league = $location.path().split('/')[1];
+
+            if (vm.league) {
+                dataservice.getResultForLeague(vm.league)
+                    .then(function (data) {
+                        vm.results = angular.fromJson(data[0].Result);
+                        vm.calculated = data[0].CalculatedAt;
+                    })
+                    .finally(function(){
+                        vm.isBusy = false;
+                    });
+            } else {
+                dataservice.getLeagues()
+                    .then(function (data) {
+                        vm.leagues = data;
+                    })
+                    .finally(function(){
+                        vm.isBusy = false;
+                    });;
             }
         }
-        
-        function dataservice($q, $http){
-            return {
-                getLeagues : _.once(getLeagues),
-		        getResultForLeague : _.memoize(getResultForLeague)
-            }
-            
-            function getLeagues(league){     
-                return $q(function(resolve, reject) {                        
-                    $http.get('http://tournament.azurewebsites.net/api/tournament/EM2016/league')
-                        .then(function(response){                        
-                            resolve(response.data);
-                        });
-                });
-            } 
-            
-            function getResultForLeague(league){     
-                return $q(function(resolve, reject) {                        
-                    $http.get('http://tournament.azurewebsites.net/api/tournament/EM2016/league/' + league +'/result')
-                        .then(function(response){                        
-                            resolve(response.data);
-                        });
-                });
-            }           
-        }    
+    }
+
+    function dataservice($q, $http) {
+        return {
+            getLeagues: _.once(getLeagues),
+            getResultForLeague: _.memoize(getResultForLeague)
+        }
+
+        function getLeagues(league) {
+            return $q(function (resolve, reject) {
+                $http.get('http://tournament.azurewebsites.net/api/tournament/EM2016/league')
+                    .then(function (response) {
+                        resolve(response.data);
+                    });
+            });
+        }
+
+        function getResultForLeague(league) {
+            return $q(function (resolve, reject) {
+                $http.get('http://tournament.azurewebsites.net/api/tournament/EM2016/league/' + league + '/result')
+                    .then(function (response) {
+                        resolve(response.data);
+                    });
+            });
+        }
+    }
 })();
